0e3bf58c4c5f88ee864dcca76f243dbbd8b9ea9d
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-06-13 02:26:26 +0530

    uwu

---
+++ methods/delete-req.js
@@ -0,0 +1,3 @@
+module.exports = function (req, res) {
+  res.send('Delete request to the homepage');
+}
---
+++ methods/post-req.js
@@ -0,0 +1,3 @@
+module.exports = function (req, res) {
+    res.send('POST request to the homepage');
+  }
